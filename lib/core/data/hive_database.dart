d0816fecdf6c2f9f5650b926ba135346e2298b13
momosamir007-hash <momosamir007@gmail.com>
2026-08-29 23:24:18 +0100

    Refactor Hive adapter registration and box opening

--- lib/core/data/hive_database.dart
+++ lib/core/data/hive_database.dart
@@ -1,41 +1,55 @@
 import 'package:hive_flutter/hive_flutter.dart';
+
 import '../../features/product/data/models/product_model.dart';
 import '../../features/shop/data/models/shop_model.dart';
-// السطر الجديد لاستدعاء نموذج الفاتورة (الذي سننشئه في الخطوة القادمة)
 import '../../features/billing/data/models/sale_model.dart';
 
 class HiveDatabase {
   static const String productBoxName = 'products';
   static const String shopBoxName = 'shop';
   static const String settingsBoxName = 'settings';
-  // اسم الصندوق الجديد الخاص بالمبيعات
   static const String saleBoxName = 'sales';
 
   static Future<void> init() async {
     await Hive.initFlutter();
 
-    // Register Adapters
-    Hive.registerAdapter(ProductModelAdapter());
-    Hive.registerAdapter(ShopModelAdapter());
-    
-    // تسجيل محولات الفواتير ومحتوياتها
-    Hive.registerAdapter(SaleModelAdapter());
-    Hive.registerAdapter(SaleItemModelAdapter());
+    // Register Hive adapters only once.
+    if (!Hive.isAdapterRegistered(0)) {
+      Hive.registerAdapter(ProductModelAdapter());
+    }
 
-    // Open Boxes
+    if (!Hive.isAdapterRegistered(1)) {
+      Hive.registerAdapter(ShopModelAdapter());
+    }
+
+    if (!Hive.isAdapterRegistered(3)) {
+      Hive.registerAdapter(SaleModelAdapter());
+    }
+
+    if (!Hive.isAdapterRegistered(4)) {
+      Hive.registerAdapter(SaleItemModelAdapter());
+    }
+
+    // Open boxes.
     await Hive.openBox<ProductModel>(productBoxName);
     await Hive.openBox<ShopModel>(shopBoxName);
-    await Hive.openBox(settingsBoxName); 
-    
-    // فتح صندوق المبيعات
+    await Hive.openBox(settingsBoxName);
     await Hive.openBox<SaleModel>(saleBoxName);
   }
 
-  static Box<ProductModel> get productBox =>
-      Hive.box<ProductModel>(productBoxName);
-  static Box<ShopModel> get shopBox => Hive.box<ShopModel>(shopBoxName);
-  static Box get settingsBox => Hive.box(settingsBoxName);
-  
-  // جلب صندوق المبيعات للتعامل معه في التطبيق
-  static Box<SaleModel> get saleBox => Hive.box<SaleModel>(saleBoxName);
+  static Box<ProductModel> get productBox {
+    return Hive.box<ProductModel>(productBoxName);
+  }
+
+  static Box<ShopModel> get shopBox {
+    return Hive.box<ShopModel>(shopBoxName);
+  }
+
+  static Box get settingsBox {
+    return Hive.box(settingsBoxName);
+  }
+
+  static Box<SaleModel> get saleBox {
+    return Hive.box<SaleModel>(saleBoxName);
+  }
 }
