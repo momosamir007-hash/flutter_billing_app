ce433e9f759abecfbc67f5a00c51e8a2b145f8b9
momosamir007-hash <momosamir007@gmail.com>
2026-08-29 19:32:48 +0100

    Refactor BillingState to remove printing properties

--- lib/features/billing/presentation/bloc/billing_state.dart
+++ lib/features/billing/presentation/bloc/billing_state.dart
@@ -3,33 +3,40 @@ part of 'billing_bloc.dart';
 class BillingState extends Equatable {
   final List<CartItem> cartItems;
   final String? error;
-  final bool isPrinting;
-  final bool printSuccess;
+  final bool saleSuccess;
 
   const BillingState({
     this.cartItems = const [],
     this.error,
-    this.isPrinting = false,
-    this.printSuccess = false,
+    this.saleSuccess = false,
   });
 
-  double get totalAmount => cartItems.fold(0, (sum, item) => sum + item.total);
+  double get totalAmount {
+    return cartItems.fold(
+      0.0,
+      (sum, item) => sum + item.total,
+    );
+  }
 
   BillingState copyWith({
     List<CartItem>? cartItems,
     String? error,
     bool clearError = false,
-    bool? isPrinting,
-    bool? printSuccess,
+    bool? saleSuccess,
+    bool clearSaleSuccess = false,
   }) {
     return BillingState(
       cartItems: cartItems ?? this.cartItems,
       error: clearError ? null : (error ?? this.error),
-      isPrinting: isPrinting ?? this.isPrinting,
-      printSuccess: printSuccess ?? this.printSuccess,
+      saleSuccess:
+          clearSaleSuccess ? false : (saleSuccess ?? this.saleSuccess),
     );
   }
 
   @override
-  List<Object?> get props => [cartItems, error, isPrinting, printSuccess];
+  List<Object?> get props => [
+        cartItems,
+        error,
+        saleSuccess,
+      ];
 }
